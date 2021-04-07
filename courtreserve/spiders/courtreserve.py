53a7ec8c5a47704c309d23b4f7688062ba7d4f1b
Nick Capps <nickcapps10@gmail.com>
2021-04-07 15:19:10 +0000

    Login

--- courtreserve/spiders/courtreserve.py
+++ courtreserve/spiders/courtreserve.py
@@ -1,18 +1,29 @@
 from os import getenv
 from dotenv import load_dotenv
-from scrapy import Spider
+from scrapy import Spider, FormRequest
 
 # Load environment variables from .env
 load_dotenv()
 ORG_ID = getenv("ORGANIZATION_ID")
+USERNAME = getenv("USERNAME")
+PASSWORD = getenv("PASSWORD")
 
 
 class CourtReserveSpider(Spider):
     name = "courtreserve"
     allowed_domains = ["app.courtreserve.com"]
-    start_urls = [f"https://app.courtreserve.com/Online/Portal/Index/{ORG_ID}"]
+    start_urls = [f"https://app.courtreserve.com/Online/Account/LogIn/{ORG_ID}"]
 
     def parse(self, response):
+        return FormRequest.from_response(
+            response,
+            formid="loginForm",
+            formdata={"UserNameOrEmail": USERNAME, "Password": PASSWORD},
+            clickdata={"type": "button", "onclick": "submitLoginForm()"},
+            callback=self.after_login,
+        )
+
+    def after_login(self, response):
         print("\n")
         print("HTTP STATUS: " + str(response.status))
         print(response.css("h3::text").get())
