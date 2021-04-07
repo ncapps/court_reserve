92c6fc408d6384830af7432435737b7fd617e10a
Nick Capps <nickcapps10@gmail.com>
2021-04-07 14:10:30 +0000

    Enable formatting with black on save

--- courtreserve/spiders/courtreserve.py
+++ courtreserve/spiders/courtreserve.py
@@ -4,16 +4,16 @@ from scrapy import Spider
 
 # Load environment variables from .env
 load_dotenv()
-ORG_ID = getenv('ORGANIZATION_ID')
+ORG_ID = getenv("ORGANIZATION_ID")
 
 
 class CourtReserveSpider(Spider):
-    name = 'courtreserve'
-    allowed_domains = ['app.courtreserve.com']
-    start_urls = [f'https://app.courtreserve.com/Online/Portal/Index/{ORG_ID}']
+    name = "courtreserve"
+    allowed_domains = ["app.courtreserve.com"]
+    start_urls = [f"https://app.courtreserve.com/Online/Portal/Index/{ORG_ID}"]
 
     def parse(self, response):
         print("\n")
-        print("HTTP STATUS: "+str(response.status))
+        print("HTTP STATUS: " + str(response.status))
         print(response.css("h3::text").get())
         print("\n")
