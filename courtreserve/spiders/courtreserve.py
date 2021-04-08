101633b60317c3765ec38a6d3646926e1a0c3584
Nick Capps <nickcapps10@gmail.com>
2021-04-08 15:52:51 +0000

    Chlose spider when login fails

--- courtreserve/spiders/courtreserve.py
+++ courtreserve/spiders/courtreserve.py
@@ -1,30 +1,41 @@
 from os import getenv
+
 from dotenv import load_dotenv
-from scrapy import Spider, FormRequest
+from scrapy import Spider, Request, FormRequest
+from scrapy.exceptions import CloseSpider
 
 # Load environment variables from .env
 load_dotenv()
 ORG_ID = getenv("ORGANIZATION_ID")
 USERNAME = getenv("USERNAME")
 PASSWORD = getenv("PASSWORD")
+LOGIN_URL = f"https://app.courtreserve.com/Online/Account/LogIn/{ORG_ID}"
 
 
 class CourtReserveSpider(Spider):
     name = "courtreserve"
     allowed_domains = ["app.courtreserve.com"]
-    start_urls = [f"https://app.courtreserve.com/Online/Account/LogIn/{ORG_ID}"]
 
-    def parse(self, response):
+    def start_requests(self):
+        yield Request(LOGIN_URL, self.login)
+
+    def login(self, response):
         return FormRequest.from_response(
             response,
             formid="loginForm",
             formdata={"UserNameOrEmail": USERNAME, "Password": PASSWORD},
             clickdata={"type": "button", "onclick": "submitLoginForm()"},
-            callback=self.after_login,
+            callback=self.check_login_result,
         )
 
-    def after_login(self, response):
-        print("\n")
-        print("HTTP STATUS: " + str(response.status))
-        print(response.css("h3::text").get())
-        print("\n")
+    def check_login_result(self, response):
+        # Redirection to portal home is expected on successful login
+        if response.url == LOGIN_URL:
+            self.logger.error("Login failed. Check username and password.")
+            raise CloseSpider("Failed to login")
+        else:
+            print("\n")
+            print("HTTP STATUS: " + str(response.status))
+            print(f"Response from: {response.url}")
+            print(response.css("h3::text").get())
+            print("\n")
