9e691c1a08f754c44063947c49dbba3272e8c5f7
Nick Capps <nickcapps10@gmail.com>
2021-04-26 22:42:50 +0000

    Get verification token

--- court_reserve/spider.py
+++ court_reserve/spider.py
@@ -3,8 +3,7 @@
 import re
 import json
 import os
-from collections import defaultdict
-from time import strptime, strftime, localtime
+from urllib.parse import quote
 from pprint import pp
 
 from scrapy import Spider, Request, FormRequest
@@ -83,8 +82,10 @@ class CourtReserveSpider(Spider):
             cb_kwargs["session_id"] = re.search("sId=([0-9]+)", bookings_path).group(1)
             self.logger.debug(f"Session id: {cb_kwargs['session_id']}")
 
-            headers = get_http_headers(CONFIG["ORG_ID"], cb_kwargs["session_id"])
-            self.logger.debug(f"Request headers: {headers}")
+            cb_kwargs["headers"] = get_http_headers(
+                CONFIG["ORG_ID"], cb_kwargs["session_id"]
+            )
+            self.logger.debug(f"Request headers: {cb_kwargs}")
 
             cb_kwargs["booking_date"] = get_booking_date(
                 int(self.settings["DAYS_OFFSET"]), self.settings["TIMEZONE"]
@@ -106,7 +107,7 @@ class CourtReserveSpider(Spider):
             return Request(
                 url=f"{BASE_URL}/Reservations/ReadExpanded/{CONFIG['ORG_ID']}",
                 method="POST",
-                headers=headers,
+                headers=cb_kwargs["headers"],
                 body=f"jsonData={body}",
                 cb_kwargs=cb_kwargs,
             )
@@ -131,84 +132,30 @@ class CourtReserveSpider(Spider):
 
             # Find open court
             open_court = find_open_court(bookings, preferences)
-            self.logger.debug(
-                f"{open_court[0]} is open "
-                f'from {open_court[1].strftime("%I:%M %p")} '
-                f'to {open_court[2].strftime("%I:%M %p")}'
-            )
             if not open_court:
                 raise CloseSpider("Open court not found.")
 
-        return None
-
-    def parse_bookings(self, response):
-        reserve_date = self.settings.get("BOOKING_DATE")
-
-        json_response = json.loads(response.text)
-        self.logger.info(
-            f'Found {json_response["Total"]} bookings on {strftime("%A, %b %d", reserve_date)}'
-        )
-
-        time_re = re.compile("[0-9]+")
-        # Create dict of end time keys with list of court ids value
-        bookings_by_court = defaultdict(list)
-        for booking in json_response["Data"]:
-            court_id = str(booking["CourtId"])
-            start_time = localtime(
-                int(time_re.search(booking["Start"]).group(0)) / 1000
-            )
-            end_time = localtime(int(time_re.search(booking["End"]).group(0)) / 1000)
-            bookings_by_court[court_id].append((start_time, end_time))
-
-        for cid, bookings in bookings_by_court.items():
-            bookings_by_court[cid] = merge_booking_ranges(bookings)
-        self.logger.debug(bookings_by_court)
-
-        # Get settings for a given day
-        day_name = strftime("%A", reserve_date).lower()
-        reserve_day_schedule = self.settings.get("SCHEDULE").get(day_name)
-
-        # Get list of requesting times and court
-        if reserve_day_schedule:
-            reserve_requests = [
-                (
-                    (
-                        strptime(start_end[0], "%I:%M %p"),
-                        strptime(start_end[1], "%I:%M %p"),
-                    ),
-                    cid,
-                )
-                for start_end in reserve_day_schedule["request_times"]
-                for cid in reserve_day_schedule["court_ids"]
-            ]
-
-        else:
-
-            self.logger.error(f'Schedule not found for "{day_name}". Check settings.')
-            raise CloseSpider(f"No schedule for {day_name}")
-
-        # Find court availability
-        open_court = get_available_court(bookings_by_court, reserve_requests)
-        if open_court:
-            court_label = self.settings.get("COURT_LABEL").get(open_court["court_id"])
-            self.logger.info(
+            court_id, start, end = open_court
+            court_label = self.settings["COURTS"][court_id]
+            self.logger.debug(
                 f"{court_label} is open "
-                f'from {strftime("%I:%M %p", open_court["start_end"][0])} '
-                f'to {strftime("%I:%M %p", open_court["start_end"][1])}'
+                f'from {start.strftime("%I:%M %p")} '
+                f'to {end.strftime("%I:%M %p")}'
             )
-            # "start=Thu%20Apr%2022%202021%2019:00:00%20GMT-0700%20(Pacific%20Daylight%20Time)&"
-            # "end=Thu%20Apr%2022%202021%2019:30:00%20GMT-0700%20(Pacific%20Daylight%20Time)&"
-            request_url = (
-                "https://app.courtreserve.com/Online/Reservations/CreateReservationCourtsview/"
-                f'{self.settings.get("ORG_ID")}?'
-                f'start={strftime("%a %b %Y %H:%M:%S %z (%Z)", open_court["start_end"][0])}&'
-                f'end={strftime("%a %b %Y %H:%M:%S %z (%Z)", open_court["start_end"][1])}&'
-                f"courtLabel={court_label}&"
-                f"customSchedulerId={self.session_id}"
+            return Request(
+                url=(
+                    f"{BASE_URL}/Reservations/CreateReservationCourtsview/"
+                    f"{CONFIG['ORG_ID']}?"
+                    f"start={start.strftime('%a %b %d %Y %H:%M:%S GMT%z (%Z)')}&"
+                    f"end={end.strftime('%a %b %d %Y %H:%M:%S GMT%z (%Z)')}&"
+                    f"courtLabel={quote(court_label)}&"
+                    f"customSchedulerId={cb_kwargs['session_id']}"
+                ),
+                method="GET",
+                headers=cb_kwargs["headers"],
+                cb_kwargs=cb_kwargs,
             )
-            pp(request_url)
-            # return Request(url=request_url)
 
-        else:
-            self.logger.info("Open court not found")
-            raise CloseSpider("Open court not found")
+        if path == "reservations/createreservationcourtsview":
+            token = response.css("#createReservation-Form input").attrib["value"]
+            self.logger.debug(f"Verification Token: {token}")
