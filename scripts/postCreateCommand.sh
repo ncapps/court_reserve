a4bdfa7711a2220d2a98d071b3837697edc34ac2
Nick Capps <nickcapps10@gmail.com>
2021-04-28 05:27:50 +0000

    Add aws cli to dev container

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -2,3 +2,7 @@
 
 echo "pipenv shell" >> /home/vscode/.zshrc
 echo "pipenv install --dev" >> /home/vscode/.zshrc
+echo "autoload bashcompinit && bashcompinit" >> /home/vscode/.zshrc
+echo "autoload -Uz compinit && compinit" >> /home/vscode/.zshrc
+echo "compinit" >> /home/vscode/.zshrc
+echo "complete -C '/usr/local/bin/aws_completer' aws" >> /home/vscode/.zshrc
