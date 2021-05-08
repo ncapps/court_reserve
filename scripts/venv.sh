d7e52df2ebf3a5274d12f8b69ce25f626cbc3eee
Nick Capps <nick.capps@viasat.com>
2021-05-08 16:31:57 +0000

    Rename repo to court_scheduler

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-WORKSPACE="/workspaces/court_reserve"
+WORKSPACE="/workspaces/court_scheduler"
 
 if [[ ! -d $WORKSPACE/venv ]]; then
     python -m venv $WORKSPACE/venv
     source $WORKSPACE/venv/bin/activate
     python -m pip install --upgrade pip
     pip install -r $WORKSPACE/requirements.txt \
+        -r $WORKSPACE/requirements_dev.txt \
         -r $WORKSPACE/court_scheduler/court_reserve_lambda/requirements.txt
 else
     source $WORKSPACE/venv/bin/activate
