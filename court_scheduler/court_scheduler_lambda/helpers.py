700edc88734cb9d93b45704c65b983283dd4266b
Nick Capps <nickcapps10@gmail.com>
2021-05-18 15:20:56 +0000

    Fix bug when fsearching a court that has no existing reservations

--- court_scheduler/court_scheduler_lambda/helpers.py
+++ court_scheduler/court_scheduler_lambda/helpers.py
@@ -106,7 +106,10 @@ def find_open_court(bookings, preferences):
     for court, pref_start_end in preferences:
         pref_start, pref_end = pref_start_end
         conflict = False
-        for booked_start, booked_end in bookings.get(court, [])["start_end_times"]:
+        # No existing bookings found for the court
+        if not bookings.get(court):
+            return (court, pref_start, pref_end)
+        for booked_start, booked_end in bookings[court]["start_end_times"]:
             if pref_end > booked_start and pref_start < booked_end:
                 conflict = True
         if not conflict:
